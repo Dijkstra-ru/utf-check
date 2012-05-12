a4a90dedd56639763e2173b66bb9d94af923ed3e
Dijkstra <me@dijkstra.ru>
2012-05-12 23:18:26 +0600

    Выстрелил себе в ногу мерджем

--- src/main/scala/ru/dijkstra/UtfChecker.scala
+++ src/main/scala/ru/dijkstra/UtfChecker.scala
@@ -13,10 +13,9 @@ object UtfChecker {
     args.foreach { arg =>
       arg match {
         case option if option.startsWith("-") => {
-          val start = if (option.startsWith("--")) 2 else 1
           if (!option.contains("="))
             throw new Exception("Malformed parameter: " + option)
-          val key = option.substring(start, option.indexOf("="));
+          val key = option.substring(if (option.startsWith("--")) 2 else 1, option.indexOf("="));
           val value = option.substring(option.indexOf("=") + 1, option.length())
           if (key.length() == 0 || value.length() == 0)
             throw new Exception("Malformed parameter: " + option)
@@ -28,43 +27,28 @@ object UtfChecker {
     Options(masks.toList, options)
   }
 
-  //def buildPattern(masks: List[String]) = "{" + (masks reduce(_ + "," + _)) + "}"
   def matches (masks: List[String], file: String) =
-  if (masks.isEmpty) true
-  else
-  if (masks.length == 1)
-    GlobPathMatcher(masks.head)(file)
-    else
-    masks map { GlobPathMatcher(_)(file) } reduce (_ || _)
-
+    if (masks.isEmpty) true
+    else if (masks.length == 1) GlobPathMatcher(masks.head)(file)
+    else masks map { GlobPathMatcher(_)(file) } reduce (_ || _)
 
   def main(args: Array[String]) {
-    /*import scala.io.Codec
-    Codec.toUTF8("テキスト").foreach(byte => println(Integer.toBinaryString(byte & 0xff)))
-    return*/
-
-    return
-    import CheckerOpreations.checkFile
     val opt = parseArgs(args.toList)
-    //opt.masks.foreach(println(_))
-    //opt.options.foreach(println(_))
     val dir = opt.options.get("dir") match {
       case Some(x) => x
       case None => "."
     }
     val path = Path(dir)
     if (path.isFile) {
-      //println("Is file")
       val res = checkFile(path)
       if (res.isValid)
-        println(path + " is valid")
+        println(path.path + " is valid")
       else
-        println(path + " is invalid")
+        println(path.path + " is invalid")
       if (res.isBOM)
-        println(path + " has BOM")
+        println(path.path + " has BOM")
     }
     else {
-      //println("Is directory")
       var BOMs = 0
       var invalidFiles = 0
       path.descendants() foreach {
@@ -72,12 +56,12 @@ object UtfChecker {
           val res = checkFile(file)
           if (res.isBOM) {
             BOMs += 1
-            println(file + " has BOM")
-          } // else println(file + " has no BOM")
+            println(file.path + " has BOM")
+          }
           if (!res.isValid) {
             invalidFiles += 1
-            println(file + " is not utf8-correct")
-          } // else println(file + " is correct")
+            println(file.path + " is not utf8-correct")
+          }
         }
       }
       if (invalidFiles == 0)
@@ -86,7 +70,6 @@ object UtfChecker {
       if (BOMs == 0)
         println("No files with BOM found")
       else println("Total files with BOM: " + invalidFiles)
-
     }
   }
 }
